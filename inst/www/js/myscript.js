4390c5c4bde4556e212688beb2f9679ba142550b
Ines Bahram <inesbahram@gmail.com>
2015-04-21 16:08:30 +0200

    First commit

---
+++ inst/www/js/myscript.js
@@ -0,0 +1,10 @@
+String.prototype.escapeSpecialChars = function() {
+    return this.replace(/\\n/g, "\\n")
+               .replace(/\\'/g, "\\'")
+               .replace(/\\"/g, '\\"')
+               .replace(/\\&/g, "\\&")
+               .replace(/\\r/g, "\\r")
+               .replace(/\\t/g, "\\t")
+               .replace(/\\b/g, "\\b")
+               .replace(/\\f/g, "\\f");
+};
