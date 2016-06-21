a57ce8321b6e5e65b8c7e2bbb52f7a134f337c32
win-stub <nturenne.inra@yahoo.fr>
2016-06-21 17:14:47 +0700

    update a parameter for function xconfig and a function for backup file json

--- R/config.R
+++ R/config.R
@@ -1,6 +1,29 @@
 options("encoding" = "UTF-8")
-xconfig <- function()
+xconfig <- function(json_path="")
 {
+  #if let's use json_path option
+  if(json_path != "")
+  {
+    file_name = basename(json_path);
+    #get extention
+    tmp = strsplit(file_name,'[.]')[[1]];
+    ext = tmp[length(tmp)];
+    if(ext == "json")
+    {
+      destination = paste(.libPaths()[1],"/x.ent/www/config/",sep="");
+      
+      file.copy(json_path,destination,overwrite = TRUE)
+      #if file name uploaded est different with the file name ini.json of x.ent
+      if(file_name != "ini.json")
+      {
+        file.rename(file.path(destination, file_name),file.path(destination, "ini.json"))
+      }
+    }
+    else
+    {
+      print("File name must be a text file in JSON format. X.ent will use the default configuration file.")
+    }
+  }
     opencpu$browse("/library/x.ent/www/config.html")  
 }
 save_config <- function(data = "")
@@ -8,6 +31,7 @@ save_config <- function(data = "")
   if(data == ""){
     stop("Data is empty!")
   }
+  else
   {
     #message = paste("Save data successful!", R.Version()$version.string)  
     write(data,paste(.libPaths()[1],"/x.ent/www/config/ini.json",sep="")) 
